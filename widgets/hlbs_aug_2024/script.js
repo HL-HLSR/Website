acbfbf1b484d13bbeab1be3f79712335097d3a14
InfiniteC0re <inf.core0@gmail.com>
2024-08-18 19:20:03 +0500

    [counter 2024] interpolate --color with progress

--- widgets/hlbs_aug_2024/script.js
+++ widgets/hlbs_aug_2024/script.js
@@ -139,6 +139,28 @@ const formatNumber = (x) => {
     }
   }
 
+  // https://stackoverflow.com/questions/66123016/interpolate-between-two-colours-based-on-a-percentage-value
+  function interpolate(color1, color2, percent) {
+    // Convert the hex colors to RGB values
+    const r1 = parseInt(color1.substring(1, 3), 16);
+    const g1 = parseInt(color1.substring(3, 5), 16);
+    const b1 = parseInt(color1.substring(5, 7), 16);
+  
+    const r2 = parseInt(color2.substring(1, 3), 16);
+    const g2 = parseInt(color2.substring(3, 5), 16);
+    const b2 = parseInt(color2.substring(5, 7), 16);
+
+    percent = Math.min(Math.max(percent, 0.0), 1.0);
+  
+    // Interpolate the RGB values
+    const r = Math.round(r1 + (r2 - r1) * percent);
+    const g = Math.round(g1 + (g2 - g1) * percent);
+    const b = Math.round(b1 + (b2 - b1) * percent);
+  
+    // Convert the interpolated RGB values back to a hex color
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
   function Update() {
     async function MakeRequest() {
       const res = await fetch(
@@ -155,6 +177,8 @@ const formatNumber = (x) => {
         peakValue = window.debug.peak;
       }
 
+      peakValue = Math.max(peakValue, playerCount);
+
       // let playerCount = 5639;
 
       if (peakValue >= settings.goals.low) {
@@ -170,15 +194,16 @@ const formatNumber = (x) => {
         appElement.classList.add("shake");
       }
 
-      if (playerCount >= settings.goals.high && playerCount >= peakValue) {
+      if (playerCount >= settings.goals.ultrahigh && playerCount >= peakValue) {
         appElement.classList.add("glitch");
-        appElement.classList.add("shake");
-        document.documentElement.style.setProperty("--color", "#00b4ff");
       } else {
         // DO not disable shake animation here
         appElement.classList.remove("glitch");
-        document.documentElement.style.setProperty("--color", originalColor);
       }
+      
+      // Update color of the circle and text
+      let globalColor = interpolate("#ffffff", "#00b4ff", playerCount / settings.goals.ultrahigh);
+      document.documentElement.style.setProperty("--color", globalColor);
 
       // Uncomment this when the event is over
       // appElement.classList.add("shake");
